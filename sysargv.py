49aed62c67351897f28d7cbe447bf731f39ed391
venkat-c0 <42035360+venkat-c0@users.noreply.github.com>
2021-10-21 19:48:53 +0530

    Update sysargv.py
    another print statement

--- sysargv.py
+++ sysargv.py
@@ -3,4 +3,5 @@ argument_list = sys.argv
 print("The script has the name",'"',(sys.argv[0]),'"')
 print("Number of arguments is",(len(sys.argv)))
 print("The Arguments are",argument_list)
-print("End of the program");
+print("End of the program")
+print("Print statement by the venkt-c0")
