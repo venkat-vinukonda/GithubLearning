342109c2eefc8aba4b2a776fc4708a130e12d7e2
venkat-vinukonda <92913538+venkat-vinukonda@users.noreply.github.com>
2021-10-21 13:10:31 +0530

    python object

---
+++ sysargv.py
@@ -0,0 +1,5 @@
+import sys
+argument_list = sys.argv
+print("The script has the name",'"',(sys.argv[0]),'"')
+print("Number of arguments is",(len(sys.argv)))
+print("The Arguments are",argument_list)
